f9bc3684389d656a8cd626767c6155290b77dc35
Asraful Alam <asraful.alam@mobioapp.com>
2020-04-22 16:19:53 +0600

    Self Closure Memory leak fixes using Weak and Unowned

---
+++ SelfClosureMemoryLeak/SecondViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SecondViewController.swift
+//  SelfClosureMemoryLeak
+//
+//  Created by Steve JobsOne on 4/22/20.
+//  Copyright © 2020 Steve JobsOne. All rights reserved.
+//
+
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    let tutorialLink = "https://medium.com/@hari.keerthipati/self-closure-memory-leak-fixes-using-weak-and-unowned-658e2cebec2"
+    
+    @IBOutlet weak var infoLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        infoLabel.text = tutorialLink
+        NotificationCenter.default.addObserver(forName: NSNotification.Name("Go new Message"), object: nil, queue: .main) {[weak self] (notification) in
+            self?.infoLabel.text = "Go new Message"
+        }
+    }
+    
+    deinit {
+        print("=====I am vacating my space=====")
+    }
+    
+
+}
